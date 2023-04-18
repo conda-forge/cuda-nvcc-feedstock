e31d04afe746aabda9c1068bacaa2f74241309c2
Robert Maynard <rmaynard@nvidia.com>
2023-04-18 13:58:30 -0400

    Make `${PREFIX}/bin/nvcc` a symlink so that CMake can use the compiler
    CMake scans the verbose output of the compiler and requires
    a root directory that contains `bin`, `lib`, and `include`.

--- recipe/activate.sh
+++ recipe/activate.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-if [ -z "${CXX+x}" ]; then echo "Please add the C++ compiler to the environment"; exit 1; fi
-
-export NVCC_PREPEND_FLAGS_BACKUP="${NVCC_PREPEND_FLAGS}"
-export NVCC_PREPEND_FLAGS="${NVCC_PREPEND_FLAGS} -ccbin ${CXX}"
+if [[ -n "$CXX" ]]; then
+    export NVCC_PREPEND_FLAGS_BACKUP="${NVCC_PREPEND_FLAGS}"
+    export NVCC_PREPEND_FLAGS="${NVCC_PREPEND_FLAGS} -ccbin=${CXX}"
+fi
+if [ -z "${CXX+x}" ]; then echo 'cuda-nvcc: Please add the `c-compiler` and `cxx-compiler` packages to the environment.'; fi
