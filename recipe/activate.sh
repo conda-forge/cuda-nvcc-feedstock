c280c2545eaf8f68e69cb1d0982e0bb228858aea
adibbley <103537006+adibbley@users.noreply.github.com>
2023-05-19 16:47:02 -0400

    Apply suggestions from code review
    Co-authored-by: jakirkham <jakirkham@gmail.com>

--- recipe/activate.sh
+++ recipe/activate.sh
@@ -15,20 +15,11 @@ then
   export CXXFLAGS_CUDA_NVCC_BACKUP="${CXXFLAGS}"
 fi
 
-if [[ ! -z "${NVCC_PREPEND_FLAGS+x}" ]]
-then
-  export NVCC_PREPEND_FLAGS_BACKUP="${NVCC_PREPEND_FLAGS}"
-fi
-
 [[ "@cross_target_platform@" == "linux-64" ]] && targetsDir="targets/x86_64-linux"
 [[ "@cross_target_platform@" == "linux-ppc64le" ]] && targetsDir="targets/ppc64le-linux"
 [[ "@cross_target_platform@" == "linux-aarch64" ]] && targetsDir="targets/sbsa-linux"
 
 if [ "${CONDA_BUILD:-0}" = "1" ]; then
-    if [ -z "${CXX+x}" ]; then
-        echo 'cuda-nvcc: Please add the `compiler("c")` and `compiler("cxx")` packages to the environment.'
-        exit 1
-    fi
     CUDA_INCLUDE_DIR="${PREFIX}/${targetsDir}/include"
     # Needed to fix cross compilation
     export CMAKE_ARGS="${CMAKE_ARGS} -DCMAKE_FIND_ROOT_PATH=$PREFIX;$BUILD_PREFIX/$HOST/sysroot;$BUILD_PREFIX/${targetsDir}"
@@ -42,4 +33,13 @@ fi
 export CFLAGS="${CFLAGS} -I${CUDA_INCLUDE_DIR}"
 export CPPFLAGS="${CPPFLAGS} -I${CUDA_INCLUDE_DIR}"
 export CXXFLAGS="${CXXFLAGS} -I${CUDA_INCLUDE_DIR}"
-export NVCC_PREPEND_FLAGS="${NVCC_PREPEND_FLAGS} -ccbin=${CXX}"
+if [ -z "${CXX+x}" ]; then
+    echo 'cuda-nvcc: Please add the `compiler("c")` and `compiler("cxx")` packages to the environment.'
+    false
+else
+    if [[ ! -z "${NVCC_PREPEND_FLAGS+x}" ]]
+    then
+        export NVCC_PREPEND_FLAGS_BACKUP="${NVCC_PREPEND_FLAGS}"
+    fi
+    export NVCC_PREPEND_FLAGS="${NVCC_PREPEND_FLAGS} -ccbin=${CXX}"
+fi
