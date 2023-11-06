3c745901fc1f93c47ebabcd4a54d821457ba22f0
adibbley <adibbley@nvidia.com>
2023-11-06 15:54:19 -0500

    Syntax

--- recipe/deactivate.bat
+++ recipe/deactivate.bat
@@ -3,5 +3,5 @@
 :: Restore environment variables (if there is anything to restore)
 if defined INCLUDE_CONDA_NVCC_BACKUP (
 	set "INCLUDE=%INCLUDE_CONDA_NVCC_BACKUP%"
-	set "INCLUDE_CONDA_NVCC_BACKUP ="
+	set "INCLUDE_CONDA_NVCC_BACKUP="
 )
