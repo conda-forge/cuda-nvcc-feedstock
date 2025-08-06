a18a019909cf8d597a61b3453274e4d6aaf04df9
Bradley Dice <bdice@bradleydice.com>
2025-08-06 12:44:00 -0500

    Use C++17 in Linux tests

--- recipe/run_nvcc_tests.sh
+++ recipe/run_nvcc_tests.sh
@@ -27,7 +27,7 @@ cmake_version=$(cmake --version | grep version | awk '{print $3}')
 
 nvcc --version
 
-$CXX --verbose ${CXXFLAGS} test.cpp ${LDFLAGS} -lcuda -lcudart_static
+$CXX --verbose -std=c++17 ${CXXFLAGS} test.cpp ${LDFLAGS} -lcuda -lcudart_static
 
 nvcc --verbose test.cu
 
